c002b54c91556ecd41c61f5b0c3b4b5a3d1d16b0
Tim Smith <tim.tim21@hotmail.co.uk>
2022-05-09 12:10:37 +0100

    basic functionality working

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const add = (x, y) => x + y;
-const subtract = (x, y) => y - x;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => y / x;
-const operate = (operator, x, y) => operator(x, y);
-const clear = () => {
-  updateDisplay([0]);
-  arr = [];
-  currentNum = [];
+const add = (x, y) => Math.round(100 * (x + y)) / 100;
+const subtract = (x, y) => Math.round(100 * (x - y)) / 100;
+const multiply = (x, y) => Math.round(100 * (x * y)) / 100;
+const divide = (x, y) => {
+  return y === 0 ? 'PROBLEMO!' : Math.round(100 * (x / y)) / 100;
 };
+const operate = (operator, x, y) => operator(x, y);
 
 let displayValue;
 const display = document.querySelector('.calc-display');
@@ -15,6 +12,8 @@ const numberBtns = document.querySelectorAll('.num-btn');
 const operators = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('.clear-btn');
 const equalBtn = document.querySelector('.equal-btn');
+const decimalBtn = document.querySelector('.decimal-btn');
+
 const obj = {
   add: add,
   subtract: subtract,
@@ -22,45 +21,76 @@ const obj = {
   divide: divide,
 };
 
-let arr = [];
+let masterArr = [];
 let currentNum = [];
 let currentOperator = '';
-let prevOperator = currentOperator;
-let currentResult;
+let lastPressed = '';
 
-const updateDisplay = (arr) => {
-  display.value = arr.join('');
-  displayValue = Number(arr.join(''));
+const calculateResult = (arr) => {
+  let result;
+  let operation = arr.find((value) =>
+    /(add|divide|multiply|subtract)/g.test(value)
+  );
+  if (operation == undefined) {
+    result = Number(arr.slice(0).join(''));
+  } else {
+    let num1 = Number(arr.slice(0, arr.indexOf(operation)).join(''));
+    let num2 = Number(arr.slice(arr.indexOf(operation) + 1).join(''));
+    result = obj[operation](num1, num2);
+  }
+  updateDisplay([result]);
+  masterArr = [result];
+  decimalBtn.disabled = false;
+};
+
+const updateDisplay = (arrayOfNum) => {
+  display.value = arrayOfNum.join('');
+};
+
+const clear = () => {
+  updateDisplay([0]);
+  masterArr = [];
+  currentNum = [];
+  decimalBtn.disabled = false;
 };
 
 clearBtn.addEventListener('click', clear);
 
 numberBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    let numClicked = parseInt(btn.dataset.num);
+    if (lastPressed === 'equals') {
+      masterArr = [];
+      currentNum = [];
+    }
+    if (masterArr.includes('.')) {
+      decimalBtn.disabled = true;
+    }
+    let numClicked = btn.dataset.num;
+    masterArr.push(numClicked);
     currentNum.push(numClicked);
     updateDisplay(currentNum);
+    lastPressed = 1;
   });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener('click', (e) => {
-    currentNum = Number(currentNum.join(''));
-    arr.push(currentNum);
-    currentOperator = operator.dataset.type;
-    currentResult =
-      arr.length > 1
-        ? operate(obj[prevOperator], currentNum, arr[arr.length - 2])
-        : currentNum;
-    updateDisplay([currentResult]);
+    calculateResult(masterArr);
     currentNum = [];
-    prevOperator = currentOperator;
-    arr = [];
-    arr.push(currentResult);
+    let clickedOperator = operator.dataset.type;
+    masterArr.push(clickedOperator);
+    lastPressed = 1;
   });
 });
 
 equalBtn.addEventListener('click', (e) => {
-  currentResult = operate(obj[currentOperator], ...currentNum, ...arr);
-  updateDisplay([currentResult]);
+  calculateResult(masterArr);
+  currentNum = [];
+  lastPressed = 'equals';
+});
+
+decimalBtn.addEventListener('click', (e) => {
+  if (masterArr.includes('.')) {
+    decimalBtn.disabled = true;
+  }
 });
